2b5a2ad04338d42b7f49bd2e6ad9424e82d79f52
lamanchy <lamanchy@gmail.com>
2022-01-10 13:28:59 +0100

    faster fish generation (was to easy)

--- main.py
+++ main.py
@@ -61,7 +61,6 @@ def main():
                     else:
                         image = pygame.image.load("images/biggest_fishRed.png")
                     ComputerFish(image, position, size)
-                    break
 
         running = handle_events(player_fish, running)
 
